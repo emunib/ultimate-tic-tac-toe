b8073aa1299414918b62d113c57cc2d2bd0b0d5d
Ehtasham Munib <emunib@outlook.com>
2023-06-17 18:10:46 -0600

    made a 3x3 grid of individual tic-tac-toe games with new state to keep track of game winners; moved checkWinner() function to App as a result.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import {useState} from 'react';
 import Board from './components/Board';
 import './style.scss';
 
 function App() {
+    const [boards, setBoards] = useState(Array(9).fill(null));
+
+    function checkWinner(board, values) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (values[a] && values[a] === values[b] && values[a] === values[c]) {
+                const nextBoards = [...boards];
+                nextBoards[board] = values[a];
+                setBoards(nextBoards);
+            }
+        }
+    }
+
     return (
-        <Board/>
+        <>
+            {boards.map((board, i) => <Board key={i} winner={board} checkWinner={(values) => checkWinner(i, values)}/>)}
+        </>
     );
 }
 
--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -2,10 +2,9 @@ import React, {useState} from 'react';
 import Box from '../Box';
 import './style.scss';
 
-function Board() {
+function Board({winner, checkWinner}) {
     const [values, setValues] = useState(Array(9).fill(null));
     const [isXTurn, setIsXTurn] = useState(true);
-    const [winner, setWinner] = useState(null);
 
     function handleClick(i) {
         if (winner || values[i]) return;
@@ -18,32 +17,10 @@ function Board() {
         setIsXTurn(isXTurn => !isXTurn);
     }
 
-    function checkWinner(values) {
-        const lines = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6]
-        ];
-        for (let i = 0; i < lines.length; i++) {
-            const [a, b, c] = lines[i];
-            if (values[a] && values[a] === values[b] && values[a] === values[c]) {
-                setWinner(values[a]);
-            }
-        }
-    }
-
     return (
-        <>
-            <p>Winner: {winner}</p>
-            <div className="board">
-                {values.map((val, i) => <Box key={i} value={val} onBoxClick={() => handleClick(i)}/>)}
-            </div>
-        </>
+        <div className="board">
+            {values.map((val, i) => <Box key={i} value={val} onBoxClick={() => handleClick(i)}/>)}
+        </div>
     );
 }
 
